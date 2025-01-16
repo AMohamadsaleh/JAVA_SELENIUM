04bc23329ab1ff2e41998fccedc805ef6e33f71c
AMohamadsaleh <147268115+AMohamadsaleh@users.noreply.github.com>
2025-01-16 22:25:50 +0700

    Update Main.java

--- src/day1/Main.java
+++ src/day1/Main.java
@@ -13,6 +13,8 @@ public class Main {
 
         System.out.println("Created BankAccount object...");
         System.out.println("Account Balance: " + myacc.getBalance());
+        myacc.withdraw(500);
+        System.out.println("Account Balance: " + myacc.getBalance());
         System.out.println("Account name: " + myacc.getName());
         System.out.println("Account age: " + myacc.getAge());
         System.out.println("Account email: " + myacc.getEmail());
