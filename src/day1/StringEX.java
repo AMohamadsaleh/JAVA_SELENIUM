b8a0150c741366c244c0c4cf46efb94d142bff20
Aziz <abdulazizmohamadsaleh@gmail.com>
2025-02-02 05:03:34 +0700

    good

---
+++ src/day1/StringEX.java
@@ -0,0 +1,22 @@
+package day1;
+
+public class StringEX {
+    public static void main(String[] args) {
+    String a = " Aziz welcome ";
+    String b = "Mohamad";
+        System.out.println(a.contains("m"));
+
+        System.out.println(a.charAt(2));
+        System.out.println(a);
+        System.out.println(a.trim());
+
+
+        System.out.println(a.trim().equals("Aziz"));
+
+        System.out.println(a.substring(9,13));
+
+        int test = 1234;
+        String q = "aziz";
+
+    }
+}
